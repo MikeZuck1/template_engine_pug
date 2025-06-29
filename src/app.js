cdd15ee2668182bd26f3ad5dce07eabf9edd88e8
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-06-29 23:45:03 +0200

    add admin route.

--- src/app.js
+++ src/app.js
@@ -3,11 +3,26 @@ const app = express();
 const path = require("path");
 
 app.set("view engine", "pug");
-
 app.set("views", path.join(__dirname, "../views"));
 
 app.get("/", (req, res) => {
   res.status(200).render("home");
+});
+
+app.get(['/admin', '/ceo', '/cto'], (req, res) => {
+  if(req.url === '/admin' || '/ceo' || '/cto') {
+    return res.status(200).render("admin");
+  } else {
+    return res.status(500); 
+  }
+});
+
+app.get('/users', (req, res) => {
+  if(req.url === '/users') {
+    return res.status(200).render("users");
+  } else {
+    return res.status(500);
+  }
 });
 
 app.use((req, res) => {
