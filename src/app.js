5ea33827bebd997e8c2df6791432558b0a65cab2
Mike Batelahoko <mikebatelahoko96@gmail.com>
2025-06-30 17:22:42 +0200

    worked on parameters.

--- src/app.js
+++ src/app.js
@@ -18,11 +18,27 @@ app.get(["/admin", "/ceo", "/cto"], (req, res) => {
 });
 
 app.get("/users", (req, res) => {
+  console.log("users pages loaded...");
   if (req.url === "/users") {
     return res.status(200).render("users");
   } else {
     return res.status(400).send("bad request.");
   }
+});
+
+app.param("userId", (req, res, next, value, name) => {
+  const user = []; // empty array
+  const getUserFromDatabase = {
+    id: value,
+    name: "nole",
+  };
+  req.user = user.push(); // add user from the empty array.
+  console.log(getUserFromDatabase);
+  next();
+});
+
+app.get("/checkout/:userId/:userCompany", (req, res) => {
+  res.status(200).send("<h1>disconnected...</h1>");
 });
 
 app.use((req, res) => {
